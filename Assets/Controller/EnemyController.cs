abffdef2b1d0daed10016097307e45b4a3de95b8
Sergio <lsergiobp@gmail.com>
2013-10-30 07:45:32 -0400

    Completando movimento do enemy

--- Assets/Controller/EnemyController.cs
+++ Assets/Controller/EnemyController.cs
@@ -8,6 +8,9 @@ public class EnemyController : MonoBehaviour {
 	private Vector3 initialPosition;
 	private Vector3 enemyPosition;
 	private float limit;
+	private float intervalBetweenFrames = 0.2f;
+	private float enemyVelocity = 0.15f;
+	private bool isPlaying;
 	
 	void Start () {
 		initSprite();
@@ -16,27 +19,53 @@ public class EnemyController : MonoBehaviour {
 		initialPosition = transform.localPosition;
 		limit = initialPosition.y + 35.0f;
 		right = true;
+		isPlaying = false;
+		
+		if( GameEventController.playNumber == 0 )
+		{
+			renderer.enabled = false;
+		}
 	}
 	
 	void Update () {
 	
 		if ( transform.localPosition.x >= limit )
+		{
 			right = false;
+			sprite.Stop();
+			isPlaying = false;
+		}
 		
 		if ( transform.localPosition.x <= initialPosition.x )
+		{
 			right = true;
+			sprite.Stop();
+			isPlaying = false;
+		}
 		
 		if( right ) 
-		{
+		{	
+			if( !isPlaying )
+			{
+				sprite.Play( Sprite.PlayMode.WalkRight, intervalBetweenFrames );
+				isPlaying = true;
+			}
+			
 			enemyPosition = transform.localPosition;
-			enemyPosition.x = enemyPosition.x + 0.2f;
+			enemyPosition.x = enemyPosition.x + enemyVelocity;
 			transform.localPosition = enemyPosition;
 		}
 		
 		else if( !right ) 
 		{
+			if( !isPlaying )
+			{
+				sprite.Play( Sprite.PlayMode.WalkLeft, intervalBetweenFrames );
+				isPlaying = true;
+			}
+			
 			enemyPosition = transform.localPosition;
-			enemyPosition.x = enemyPosition.x - 0.2f;
+			enemyPosition.x = enemyPosition.x - enemyVelocity;
 			transform.localPosition = enemyPosition;
 		}
 		
@@ -58,6 +87,6 @@ public class EnemyController : MonoBehaviour {
 	
 	void GameStart() 
 	{
-		
+		renderer.enabled = true;
 	}
 }
